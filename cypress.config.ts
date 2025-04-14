bed59cc7f28db56cc3c87a3f5db51c749f34f95f
taijamartinez <taijasmartinez@gmail.com>
2025-04-14 13:40:04 -0400

    added yml files, updated seed and cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:3001',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
